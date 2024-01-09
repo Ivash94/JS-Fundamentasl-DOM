297608b78b6388c575c9ca76638ca2e6ae2d3284
Ivash94 <118127083+Ivash94@users.noreply.github.com>
2024-01-09 22:42:22 +0200

    Update task4.js

--- task4.js
+++ task4.js
@@ -6,7 +6,11 @@
 
 
 // Ваш код ..
-
+const form = document.querySelector(".array fieldset");
+const name = form.querySelector(`input[name = "fio"]`).value;
+const phoneNumber = form.querySelector(`input[name = "phone"]`).value;
+const dataOfBirth = form.querySelector(`input[name = "birthday"]`).value;
+const emailAddress = form.querySelector(`input[name = "email"]`).value;
 
 module.exports = {
     name,
