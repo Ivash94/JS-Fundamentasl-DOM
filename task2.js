7f546d80c61e41839a4d8c90ec942d8d9b4f812e
Ivash94 <118127083+Ivash94@users.noreply.github.com>
2024-01-09 22:42:53 +0200

    Update task2.js

--- task2.js
+++ task2.js
@@ -3,21 +3,5 @@
 // Для перевірки завдання введіть команду npm jest task2.test.js
 
 
-const header = document.querySelector("#title");
-const textPar = document.querySelector("#myDiv");
-const myList = document.querySelector("#myList");
-const invSpan = document.querySelector(".style_elements span");
-
-
-function applyStyles() {
-    header.style.backgroundColor = "green";
-    textPar.children[0].style.fontWeight = "bold";
-    textPar.children[1].style.color = "red";
-    textPar.children[2].style.textDecoration = "underline";
-    textPar.children[3].style.fontStyle = "italic";
-    myList.style.display = "flex";
-    myList.style.listStyle = "none";
-    invSpan.style.display = "none";
-}
 
 // module.exports = applyStyles;
